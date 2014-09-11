5357b900d0f4194fc1f6f74bc456d42a23df0941
mountassir <mountassir@443723c5-464c-432d-85fa-44d6fa91ea01>
2014-09-11 16:14:41 +0000

    Professeur

---
+++ NaStruts2/src/main/java/com/bean/Professeur.java
@@ -0,0 +1,35 @@
+package com.bean;
+
+public class Professeur {	
+	
+	private String nom;
+	private String prenom;
+    private Integer age;
+    private Matiere matiere;
+    public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Matiere getMatiere() {
+		return matiere;
+	}
+	public void setMatiere(Matiere matiere) {
+		this.matiere = matiere;
+	}
+
+
+}
