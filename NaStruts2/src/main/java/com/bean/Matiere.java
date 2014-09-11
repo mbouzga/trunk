50b5c5adb7b750d1949d4a34bbd90496f3b66965
mountassir <mountassir@443723c5-464c-432d-85fa-44d6fa91ea01>
2014-09-11 16:14:23 +0000

    Matiere

---
+++ NaStruts2/src/main/java/com/bean/Matiere.java
@@ -0,0 +1,27 @@
+package com.bean;
+
+public class Matiere {
+	private String nom;
+	private String filliere;
+	private String heures;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getFilliere() {
+		return filliere;
+	}
+	public void setFilliere(String filliere) {
+		this.filliere = filliere;
+	}
+	public String getHeures() {
+		return heures;
+	}
+	public void setHeures(String heures) {
+		this.heures = heures;
+	}
+	
+
+}
