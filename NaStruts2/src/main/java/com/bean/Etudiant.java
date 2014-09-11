de3962ef05d47e07149b9d7a1bac99ad49617db4
mountassir <mountassir@443723c5-464c-432d-85fa-44d6fa91ea01>
2014-09-11 16:13:59 +0000

    le bean Etudiant

---
+++ NaStruts2/src/main/java/com/bean/Etudiant.java
@@ -0,0 +1,27 @@
+package com.bean;
+
+public class Etudiant {
+	
+	private String nom;
+    private String prenom;
+    private Integer age;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
