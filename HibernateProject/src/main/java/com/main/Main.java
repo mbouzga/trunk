10e67238a45d0f4872093edc9b3d2ea6b7698cd8
mbouzga <bouzga.mountassir@gmail.com>
2016-10-06 13:58:40 +0200

    test 2

--- HibernateProject/src/main/java/com/main/Main.java
+++ HibernateProject/src/main/java/com/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(final String[] args) {
 
-    	/***/
+    	/** test git*/
         IPersonneDao personneDao = new PersonneDaoImpl();
 
         Personne personneCreation = new Personne();
